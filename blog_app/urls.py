2d556f665da96a6d237cf0a1d1f395535ae53ebf
Thomas Overment <tomozart22@outlook.com>
2024-07-16 15:44:01 +0000

    blog_app urls.py

---
+++ blog_app/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path
+from . import views
+from .views import delete_post
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    path('', views.PostList.as_view(), name='home'),
+    path('profile/', views.profile, name='profile'),
+    path('about/', views.about, name='about'),
+    path('gallery/', views.gallery, name='gallery'),
+    path('post/<slug:slug>/', views.post_detail, name='post_detail'),
+    path('addpost/', views.AddPostView.as_view(), name='add_post'),
+    path('post/edit/<int:pk>/', views.UpdatePost.as_view(), name='update_post'),
+    path('postdetail/<int:pk>/', views.PostDetail.as_view(), name='post_detail'),
+    path('blog/', views.PostList.as_view(), name='blog'),
+    path('post/<int:post_id>/delete/', delete_post, name='delete_post'),
+    
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
