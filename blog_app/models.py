4c50f4b006305c67615878dc10f449c965d0d3ea
Thomas Overment <tomozart22@outlook.com>
2024-07-16 11:51:45 +0000

    readded html models and views

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_app_posts")
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    image = models.ImageField(upload_to='images/', null=True, blank=True)  
+
+    def __str__(self):
+        return self.title + ' | ' + str(self.author)
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[self.slug])
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Comment by {self.author} on {self.post}"
+
+from django.db import models
+
+class Artwork(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='artworks/')
+
+    def __str__(self):
+        return self.title
+
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,3 +1,90 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views import generic
+from django.http import HttpResponseRedirect
+from django.contrib import messages
+from .models import Post, Comment
+from .forms import PostForm, UpdateForm, CommentForm
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
+from django.http import HttpResponse, HttpResponseForbidden
+from .models import Artwork
 
-# Create your views here.
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = "blog_app/index.html"
+    paginate_by = 6
+
+def profile(request):
+    return render(request, 'blog_app/profile.html')
+
+def about(request):
+    return render(request, 'blog_app/about.html')
+
+def gallery(request):
+    return render(request, 'blog_app/gallery.html')
+
+class AddPostView(generic.CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog_app/addpost.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.save()
+        messages.add_message(self.request, messages.SUCCESS, 'Post created successfully!')
+        return redirect('home')
+
+def post_detail(request, slug):
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+    comments = post.comments.all().order_by("-created_on")
+    comment_count = post.comments.filter(approved=True).count()
+    
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            messages.add_message(request, messages.SUCCESS, 'Comment submitted and awaiting approval')
+            return HttpResponseRedirect(request.path_info) 
+    else:
+        comment_form = CommentForm()
+    
+    return render(
+        request,
+        "blog_app/post_detail.html",
+        {
+            "post": post,
+            "comments": comments,
+            "comment_count": comment_count,
+            "form": comment_form  
+        }
+    )
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = "blog_app/post_detail.html"
+
+class UpdatePost(generic.UpdateView):
+    model = Post
+    form_class = UpdateForm
+    template_name = "blog_app/editposts.html"
+
+
+def delete_post(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    if request.user != post.author:
+        raise PermissionDenied
+    if request.method == 'POST':
+        post.delete()
+        return redirect('home')
+    return render(request, 'blog_app/post_confirm_delete.html', {'post': post})
+
+def gallery(request):
+    artworks = Artwork.objects.all()
+    return render(request, 'gallery.html', {'artworks': artworks})
+    
+
+
