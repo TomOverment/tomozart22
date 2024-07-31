5ea3144f9f009449bd76a2746e8a6fe1ca3459c2
Thomas Overment <tomozart22@outlook.com>
2024-07-31 09:38:43 +0000

    cloud

--- my_project/settings.py
+++ my_project/settings.py
@@ -14,7 +14,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 DEBUG = False
-
 ALLOWED_HOSTS = [
     '8000-tomoverment-tomozart22-7vyyd9dhvch.ws.codeinstitute-ide.net',
     '.herokuapp.com', '.gitpod.io', '*', ]
@@ -159,6 +158,12 @@ SOCIALACCOUNT_PROVIDERS = {
             'access_type': 'online',
         }
     }
+}
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
