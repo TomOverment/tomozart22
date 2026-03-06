0d0708588859fc8aa2f2eee1d6c7954831056967
TomOverment <tomozart22@outlook.com>
2026-03-06 12:38:47 +0000

    Improve WhiteNoise stability

--- my_project/settings.py
+++ my_project/settings.py
@@ -145,6 +145,7 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+WHITENOISE_MANIFEST_STRICT = False
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
